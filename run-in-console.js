8c784581705479a7e9fc28cebdaf3cad7cc2b7d4
eirikb <eirikb@eirikb.no>
2017-02-03 13:41:41 +0100

    Ensure IE-support
    Code will be run through babel and added to readme.
    The generated code can be copied and used in console

--- run-in-console.js
+++ run-in-console.js
@@ -1,6 +1,14 @@
-(window.define || {}).amd = false
-  add = url => { s = document.createElement('script'); s.src = url; document.body.appendChild(s); return new Promise(r => s.onload = r) }
-  add('https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.1/socket.io.min.js').then(() => {
-  socket = io('http://localhost:5776')
-  socket.on('change', () => add(`http://localhost:8080/script.js?t=${Date.now()}`))
-})
+// Remove amd, it will mess up with most libraries, e.g., socket io
+(window.define || {}).amd = false;
+
+const addScript = (url, cb) => {
+  const script = document.createElement('script');
+  script.src = url;
+  document.body.appendChild(script);
+  script.onload = cb;
+};
+
+addScript('https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.1/socket.io.min.js', () => {
+  const socket = io('http://localhost:5776');
+  socket.on('change', () => addScript(`http://localhost:8080/script.js?t=${Date.now()}`));
+});
